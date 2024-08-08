3239ba123c5879ed4bb1ec264d7c57bcf183a20c
BayanAlbaghdadi0 <ham123cam12@gmail.com>
2024-08-08 15:59:34 +0200

    -add overviw section

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { GlassCard } from "./ui/GlassCard";
 import { Lodar } from "./Lodar";
+import ProductCard from "./ProductCard/ProductCard";
+import { useLocation } from "react-router";
+
 // custom hook
 import { useGetAllCategories } from "../Hooks/useGetAllCategories";
+import { useGet5ProductsFromCategory } from "../Hooks/useGet5ProductsFromCategory";
 
 export const Overview = () => {
+  const { pathname } = useLocation();
+  console.log(pathname);
   const { loading, categories } = useGetAllCategories();
-  const {category , setCategory} = useState("mens-shirts")
-  const { products, loading2 } = useGetAllCategories(category, 5, 1);
-
-  
-
+  const [category, setCategory] = useState("mens-shirts");
+  // console.log(category);
+  const { products, loading2 } = useGet5ProductsFromCategory(category, 8, 1);
+  // console.log(products);
+  // console.log(loading2);
 
   return (
     <>
@@ -21,21 +26,28 @@ export const Overview = () => {
           <ul className="menu menu-vertical flex lg:menu-horizontal bg-slate-900 opacity-80 rounded-box">
             {categories.map((category) => (
               <li key={category}>
-                <a onClick={() => setCategory(category)} >{category}</a>
+                <a onClick={() => setCategory(category)}>{category}</a>
               </li>
             ))}
           </ul>
         </div>
       )}
 
-      <div className=" flex justify-evenly bg-slate-950 flex-wrap gap-4">
-        {/* category bar */}
+      <div className=" flex justify-evenly bg-slate-950 flex-wrap gap-4 mb-4">
+        {/* Category bar */}
         {/* {CARD DISPLAY} */}
-        {[0, 1, 2, 3, 4].map((element) => (
-          <GlassCard />
-        ))}
+        {loading2 && <Lodar />}
+        {!loading2 &&
+          products.map((product) => (
+            <ProductCard
+              title={product.title}
+              price={product.price}
+              discount={product.discountPercentage}
+              rating={product.rating}
+              thumbnail={product.thumbnail}
+            />
+          ))}
       </div>
-      <q className="text-3xl">anguesa❤️</q>
     </>
   );
 };
