8859562e5c7abd3f393f5b27417ad2ee00e36387
BayanAlbaghdadi0 <ham123cam12@gmail.com>
2024-08-09 13:47:34 +0200

    -add about us section

--- src/components/AboutUS.jsx
+++ src/components/AboutUS.jsx
@@ -4,7 +4,8 @@ import { FeaturesSectionDemo } from './ui/FeatureSection'
 export const AboutUS = () => {
     // FeaturesSectionDemo
   return (
-    <div className=''>
+    <div className='m-auto'>
+      <h2 className='text-center text-2xl md:text-4xl lg:text-7xl transition-all  uppercase'> About US 3oafi</h2>
         <FeaturesSectionDemo/>
     </div>
   )
--- src/components/ui/FeatureSection.tsx
+++ src/components/ui/FeatureSection.tsx
@@ -14,9 +14,9 @@ import {
 export function FeaturesSectionDemo() {
   const features = [
     {
-      title: "Built for developers",
+      title: "Built by top developers",
       description:
-        "Built for engineers, developers, dreamers, thinkers and doers.",
+        "Experience and skills for the best product.",
       icon: <IconTerminal2 />,
     },
     {
@@ -37,8 +37,8 @@ export function FeaturesSectionDemo() {
       icon: <IconCloud />,
     },
     {
-      title: "Multi-tenant Architecture",
-      description: "You can simply share passwords instead of buying new seats",
+      title: "Multi-product store",
+      description: "Products of all kinds for all needs",
       icon: <IconRouteAltLeft />,
     },
     {
@@ -55,7 +55,7 @@ export function FeaturesSectionDemo() {
     },
     {
       title: "And everything else",
-      description: "I just ran out of copy ideas. Accept my sincere apologies",
+      description: "We serve our customers with love, loyalty and honesty.",
       icon: <IconHeart />,
     },
   ];
@@ -82,27 +82,27 @@ const Feature = ({
   return (
     <div
       className={cn(
-        "flex flex-col lg:border-r  py-10 relative group/feature hover:text-black dark:border-neutral-800",
-        (index === 0 || index === 4) && "lg:border-l dark:border-neutral-800 ",
+        "flex flex-col lg:border-r gap-2  py-10 relative group/feature  dark:border-neutral-800",
+        (index === 0 || index === 4) && "lg:border-l dark:border-neutral-800 hover:text-black",
         index < 4 && "lg:border-b dark:border-neutral-800 text-sky-700 "
       )}
     >
       {index < 4 && (
-        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-t text-sky-700  from-neutral-100 dark:from-neutral-800 to-transparent pointer-events-none" />
+        <div className=" opacity-0 hover:text-black group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-t   from-blue-200  to-gray-50 pointer-events-none" />
       )}
       {index >= 4 && (
-        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-neutral-100 dark:from-neutral-800 to-transparent pointer-events-none" />
+        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-400 absolute inset-0 h-full w-full bg-gradient-to-b to-gray-50  from-blue-300  pointer-events-none" />
       )}
       <div className="mb-4 relative z-10 px-10 text-cyan-800 font-extrabold dark:text-neutral-400">
         {icon}
       </div>
-      <div className="text-lg text-sky-700  font-bold mb-2 relative z-10 px-10 ">
+      <div className="text-lg text-sky-700    font-bold mb-2 relative z-10 px-10 ">
         <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-500 dark:bg-neutral-700 group-hover/feature:bg-blue-500 transition-all duration-200 origin-center" />
         <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-neutral-800 dark:text-neutral-100">
           {title}
         </span>
       </div>
-      <p className="text-sm  text-neutral-600   dark:text-neutral-300 max-w-xs relative z-10 px-10 ">
+      <p className="text-lg font-thin mix-blend-multiply   text-neutral-800   dark:text-neutral-900 max-w-xs relative z-10 px-10 ">
         {description}
       </p>
     </div>
